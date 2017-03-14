2f4d646a5c544a1c742dc41c45b0e7ec0a036bd5
Stéphane Wong <stephane.wong1@gmail.com>
2017-03-14 14:35:46 +0100

    get all bikes positions and data for paris

--- public/javascripts/station.js
+++ public/javascripts/station.js
@@ -2,45 +2,95 @@
  * Created by stephane on 14/03/2017.
  */
 
+const elm = React.createElement;
+
+
 var Api = React.createClass({
 
-    propTypes: {
+    /*getDefaultProps: function () {
+        return {
+            osmUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            mapBoxAttr: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ',
+            id: 'mapid',
+            popupsLayer: new L.LayerGroup(),
+            streets: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                id: 'mapbox.streets',
+                attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+            }),
+            map: L.map('mapid', {
+                center: [48.856614, 2.352222],
+                zoom: 12,
+                layers: [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    id: 'mapbox.streets',
+                    attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+                })]
+            }),
+        };
+    },*/
 
-    },
-
-    getDefaultProps: function() {
-        osmUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-        mapBoxAttr: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ';
-        id: 'mapid';
-    },
-
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             city: "Paris",
             position: [48.856614, 2.352222],
-            streets: L.tileLayer(this.props.osm, {id: 'mapbox.streets', attributions: this.props.mapBoxAttr}),
-            map: null,
             cities: null,
-            stations: null
+            stations: null,
+            base: {
+              "Streets": L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {id: 'mapbox.streets', attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '})
+            },
+            popup: null,
+            map: L.map('mapid', {
+                center: [48.856614, 2.352222],
+                zoom: 12,
+                layers: [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    id: 'mapbox.streets',
+                    attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+                })]
+            }),
+            popupsLayer: new L.LayerGroup()
         }
     },
 
-    getCities: function() {
+    getCities: function () {
 
     },
 
-    componentDidMount: function() {
 
+    fillPopups: function (stations) {
+        if (this.state.stations != null) {
+            var that = this;
+            stations.map(function (station) {
+                L.marker([station.position.lat, station.position.lng]).bindPopup(station.toString()).addTo(that.state.popupsLayer);
+            });
+            this.setState({popup: { "Stations": this.state.popupsLayer}});
+            console.log(this.state.popup);
+            L.control.layers(this.state.base, this.state.popup).addTo(this.state.map);
+        }
     },
 
-    render: function() {
-        return ();
-   }
+    componentDidMount: function () {
+        var that = this;
+        $.get("http://localhost:9000/test", function (data, status, xhr) {
+            if (xhr.status == 200 && data != null) {
+                var s = new Stations(data);
+                that.setState({stations: s.stations});
+                that.fillPopups(that.state.stations);
+            }
+        });
+    },
+
+    render: function () {
+        return elm('div', {id: this.props.id}, null);
+    }
 });
 
 
+var Stations = function (stations) {
+    this.stations = stations.map(function (station) {
+        return new Station(station);
+    });
+}
 
-/*var Station = function (apiStation) {
+var Station = function (apiStation) {
     this.number = apiStation.number;
     this.contract_name = apiStation.contract_name;
     this.name = apiStation.name;
@@ -55,7 +105,7 @@ var Api = React.createClass({
     this.last_update = apiStation.last_update;
 };
 
-Station.prototype.toString = function() {
+Station.prototype.toString = function () {
     return "Number: " + this.number +
         "\nName: " + this.name +
         "\nAddress: " + this.address +
@@ -66,30 +116,11 @@ Station.prototype.toString = function() {
         "\nLast update: " + new Date(this.last_update);
 };
 
-
-var Api = function(){};
-
-var Stations = function(stations) {
-    this.stations = stations;
-};
-
-Api.prototype.updateStations = function(stations) {
-  this.stations = stations;
-};
-
-Api.prototype.getBikesFromCity = function(city) {
-    var that = this;
-    $.get("http://localhost:9000/test", function(data,status,xhr) {
-        if(xhr.status == 200 && data != null) {
-
-            that.updateStations(data);
-        }
-        /*console.log(status);
-        console.log(data);
-    });
-};
-
-var api = new Api();
-
-api.getBikesFromCity("");
-console.log(api.stations);*/
+ReactDOM.render(
+    elm(Api, {
+        osmUrl: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        mapBoxAttr: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ',
+        id: 'mapid'
+    }, null),
+    document.getElementById('container')
+);
